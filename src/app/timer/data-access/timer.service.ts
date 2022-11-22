f43c3e3463a85f312b4c0c0eb4e43ad4c80acb8c
Mint Missy <dawidkostka480@gmail.com>
2022-11-22 20:34:25 +0100

    Add timer service

--- src/app/timer/data-access/timer.service.ts
+++ src/app/timer/data-access/timer.service.ts
@@ -6,10 +6,52 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class TimerService {
+  private timers: Timer[] = [];
+  private LOCAL_STORAGE_KEY = 'timers';
+
   constructor() {}
 
-  parseTimerFromJson(json: TimerJson): Timer {
+  getTimers(): Timer[] {
+    return this.timers;
+  }
+
+  loadTimers() {
+    const timersJson = localStorage.getItem(this.LOCAL_STORAGE_KEY);
+    if (timersJson === null) {
+      return;
+    }
+
+    const timers: TimerJson[] = JSON.parse(timersJson);
+    for (const timerJson of timers) {
+      this.addTimer(this.parseTimerFromJson(timerJson));
+    }
+  }
+
+  createTimer(duration: number): Timer {
+    const timer: Timer = {
+      id: '',
+      startDate: new Date(),
+      endDate: new Date(Date.now() + duration * 1000),
+    };
+    this.addTimer(timer);
+    return timer;
+  }
+
+  removeTimer(id: string): void {
+    this.timers = this.timers.filter((timer) => timer.id !== id);
+  }
+
+  private addTimer(timer: Timer): void {
+    this.timers.push(timer);
+  }
+
+  updateLocalStorage(): void {
+    localStorage.setItem(this.LOCAL_STORAGE_KEY, JSON.stringify(this.timers));
+  }
+
+  private parseTimerFromJson(json: TimerJson): Timer {
     return {
+      id: json.id,
       startDate: new Date(json.startDate),
       endDate: new Date(json.endDate),
     };
--- src/app/timer/model/timer.model.ts
+++ src/app/timer/model/timer.model.ts
@@ -1,9 +1,11 @@
 export interface Timer {
+  id: string;
   startDate: Date;
   endDate: Date;
 }
 
 export interface TimerJson {
+  id: string;
   startDate: string;
   endDate: string;
 }
